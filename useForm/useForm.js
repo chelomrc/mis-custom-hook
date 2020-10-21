bc64696ae6a1257bfc8c25a170da79483117ef9a
Franz Rodriguez <frodriguez@its.com>
2020-10-21 13:24:22 -0400

    Including comments in hooks

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,8 +1,14 @@
 import { useState } from "react"
 
-export const useForm = ( initialState = {} ) => {
 
-    const [values, setValues] = useState(initialState)
+export const useForm = ( initialState = {} ) => {
+    
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues( initialState );
+    }
+
 
     const handleInputChange = ({ target }) => {
 
@@ -10,12 +16,9 @@ export const useForm = ( initialState = {} ) => {
             ...values,
             [ target.name ]: target.value
         });
-    }
 
-    const reset = () => {
-        setValues( initialState );
     }
 
     return [ values, handleInputChange, reset ];
-    
+
 }
